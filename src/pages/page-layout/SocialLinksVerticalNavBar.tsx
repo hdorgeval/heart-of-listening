7bf1f9d7b3fee456f265af6ac72615c4316be7f5
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-06-24 11:11:30 +0200

    fix(button): fix margin

--- src/pages/page-layout/SocialLinksVerticalNavBar.tsx
+++ src/pages/page-layout/SocialLinksVerticalNavBar.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const SocialLinksVerticalNavBar: FC = () => {
   return (
     <button
-      className="btn btn-primary badge rounded-pill navbar-toggler border border-0 position-absolute bottom-0 end-0 pb-7 pe-2"
+      className="btn btn-dark badge rounded-pill navbar-toggler border border-0 position-absolute bottom-0 end-0 mb-7 me-2"
       type="button"
       data-bs-toggle="offcanvas"
       data-bs-target="#offcanvasResponsive"
